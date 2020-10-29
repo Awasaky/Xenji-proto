c42d73d9acc262765f3e56e8f618b30b99637094
Awasaky <awasaky@gmail.com>
2020-10-29 15:42:12 +0500

    Initial commit

---
+++ field/type_ix.go
@@ -0,0 +1,40 @@
+embedded_components {
+  id: "sprite-over"
+  type: "sprite"
+  data: "tile_set: \"/assets/art.atlas\"\n"
+  "default_animation: \"X-Cross Up\"\n"
+  "material: \"/builtins/materials/sprite.material\"\n"
+  "blend_mode: BLEND_MODE_ALPHA\n"
+  ""
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
+embedded_components {
+  id: "sprite-under"
+  type: "sprite"
+  data: "tile_set: \"/assets/art.atlas\"\n"
+  "default_animation: \"X-Cross Down\"\n"
+  "material: \"/builtins/materials/sprite.material\"\n"
+  "blend_mode: BLEND_MODE_ALPHA\n"
+  ""
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
